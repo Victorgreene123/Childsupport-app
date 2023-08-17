beed08c94caf4afb5ff48f90639c2a211eae4cc1
prince-coder10 <126191793+prince-coder10@users.noreply.github.com>
2023-08-17 21:02:05 +0000

    Pending changes exported from your codespace

---
+++ childsupport-app/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import SignUp from "./components/pages/SignUp";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/signup" Component={SignUp} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
