faf6837c837ecf656a5991403249871d3a408d4a
Victorgreene123 <96913046+Victorgreene123@users.noreply.github.com>
2023-08-18 13:38:06 -0700

    Update signup.js

--- childsupport-app/src/components/signup.js
+++ childsupport-app/src/components/signup.js
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import './App';
-import './index.css'
+ import { Link } from 'react-router-dom';
+import '../App';
+import '../index.css'
 
 export default function Signup(){
     return(
