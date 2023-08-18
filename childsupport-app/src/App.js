4957a8d6d38a8cf18bfc680ae2c24bd20acc2ed9
Victorgreene123 <96913046+Victorgreene123@users.noreply.github.com>
2023-08-18 21:07:18 +0100

    Update App.js

--- childsupport-app/src/App.js
+++ childsupport-app/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter  ,Routes, Route } from "react-router-dom";
 import Home  from "./components/pages/Home";
-import SignUp from "./components/pages/SignUp";
+import SignUp from "./components/signup";
 import Login from "./components/login";
 
 
