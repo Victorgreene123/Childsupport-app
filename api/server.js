283fe9177b436a37c90b2008fa99926f0e2e527d
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2023-08-22 01:20:59 +0530

    enabling cors for cross Origin resource sharing in server.js

--- api/server.js
+++ api/server.js
@@ -4,7 +4,15 @@ const dbFunction = require("./db/database");
 const bodyParser = require("body-parser"); // to get form data
 app.use(express.json());// to send back data as response in json format
 app.use(bodyParser.urlencoded({ extended: true }));
+const cors = require("cors");
+
+// ---- DATABASE CONNECTION ---
 dbFunction();
+
+// Allowing all ORIGINS -> Will change after deployment
+app.use(cors());
+
+// checking home route
 app.get("/", (req,res)=>{
     const responseData = {
         "status" : "Healthy",
@@ -15,7 +23,7 @@ app.get("/", (req,res)=>{
 })
 
 // ------ ALll Routes ----
-// USER ROUTE( APIs : create new user,login, logout, delete user)
+// USER ROUTE( APIs : create new user, login, logout, delete user)
 app.use("/api/user",require("./routes/userRoute"));
 
 
