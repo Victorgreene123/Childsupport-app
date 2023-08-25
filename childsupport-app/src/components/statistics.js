d9804fabfcc0c79e041afb4b4b2bd988c8a733c0
Victorgreene123 <96913046+Victorgreene123@users.noreply.github.com>
2023-08-25 18:08:08 +0100

    Update statistics.js

--- childsupport-app/src/components/statistics.js
+++ childsupport-app/src/components/statistics.js
@@ -6,8 +6,8 @@
 
 
 import { Link } from 'react-router-dom';
-import './App';
-import './index.css'
+import '../App';
+import '../index.css'
 import React from 'react';
 import Sectionlink from './sectionlink';
 
